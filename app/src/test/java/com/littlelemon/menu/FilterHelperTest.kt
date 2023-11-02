76b9eb9dcf9d3cac6580f95fd174c6b220dd4651
Ashcoder9752 <ashutoshkumarsahildav@gmail.com>
2023-11-03 01:17:47 +0530

    final project

---
+++ app/src/test/java/com/littlelemon/menu/FilterHelperTest.kt
@@ -0,0 +1,34 @@
+package com.littlelemon.menu
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class FilterHelperTest {
+
+    @Test
+    fun filterProducts_filterTypeDessert_croissantReturned() {
+        val sampleProductsList = mutableListOf(
+            ProductItem(
+                title = "Black tea",
+                price = 3.00,
+                category = "Drinks",
+                R.drawable.black_tea
+            ),
+            ProductItem(
+                title = "Croissant",
+                price = 7.00,
+                category = "Dessert",
+                R.drawable.croissant
+            ),
+            ProductItem(
+                title = "Bouillabaisse",
+                price = 20.00,
+                category = "Food",
+                R.drawable.bouillabaisse
+            )
+        )
+
+        val dessert = FilterHelper().filterProducts(FilterType.Dessert, sampleProductsList)
+        assertEquals("Croissant", dessert[0].title)
+    }
+}
